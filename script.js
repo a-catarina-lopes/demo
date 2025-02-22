9f22b7e199d7c2c7215cd52c30325899b2a122c7
Ana Catarina Lopes <68606929+a-catarina-lopes@users.noreply.github.com>
2025-02-22 01:51:50 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,238 @@
+// Detailed info stored in a JavaScript object
+/////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////EQUIPA
+/////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
+
+const teamDetails = {
+  "carolina-oliveira": {
+    title: "Carolina Oliveira",
+    role: "Co-fundadora e Psicóloga",
+    description: `
+        <p> Psicóloga Clínica — Membro Efetivo da Ordem dos Psicólogos (OPP), Cédula Profissional nº24702</p>
+        <p> Mestre em Psicologia Clínica — Mestrado em Psicoterapia Sistémica e Familiar — pela Faculdade de Psicologia e Ciências da Educação da Universidade de Coimbra</p>
+        <p> Programa de Doutoramento Inter-universitário (área de especialização em Psicologia Clínica e área temática Psicologia da Família e Intervenção Familiar) - em curso</p>
+        <p> Co-Fundadora Who Cares</p>
+        <p> Formação Especializada em Intervenção Sistémica e Familiar pela Sociedade Portuguesa de Terapia Familiar (SPTF) (atualmente em supervisão)</p>
+        <p> Fulbrighter Alumni (no Chicago Center for Family Health)</p>
+        <p> Formadora Certificada nº F706104/2021</p>
+        <p> Desenvolve prática clínica em contexto privado</p>
+      `,
+    iconLink: "https://www.linkedin.com/in/carolina-oliveira-284350148/" // Unique link for this profile
+  },
+  "maria-santos": {
+    title: "Maria Santos",
+    role: "Psicóloga Clínica",
+    description: `
+        <p> Psicóloga Clínica — Membro efetivo da Ordem dos Psicólogos Portugueses, Cédula Profissional nº 26505 </p>
+        <p> Mestre em Psicologia Clínica e da Saúde com subespecialização em Psicopatologia e Psicoterapias Dinâmicas pela Faculdade de Psicologia e Ciências da Educação da Universidade de Coimbra (FPCE-UC) </p>
+        <p> Membro da Sociedade Portuguesa de terapia Familiar a realizar Supervisão em Terapia Sistémica e Familiar</p>
+        <p> Membro da British Psychological Society (BPS)</p>
+        <p> Prática clínica em contexto privado no Hospital da Luz, na SANFIL Coimbra e na WhoCares</p>
+      `,
+    iconLink: "https://www.linkedin.com/in/maria-gon%C3%A7alves-dos-santos-ab43b3177?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" // Unique link for this profile
+  },
+
+  "beatriz-almeida": {
+    title: "Beatriz Almeida",
+    role: "Psicóloga Clínica",
+    description: `
+      <p> Psicóloga — Membro efectivo da Ordem dos Psicólogos Portugueses, Cédula Profissional nº 27408</p>
+      <p> Área de especialização — Psicologia da Educação Desenvolvimento e Aconselhamento, Universidade de Coimbra</p>
+      <p> Especialização Avançada — Psicologia Clínica e da Saúde, CRIAP</p>
+      <p> Formação e maior foco nas áreas de inteligência emocional e ansiedade</p>
+    `
+  },
+  "maria-dias": {
+    title: "Maria Dias",
+    role: "Psicóloga Clínica",
+    description: `
+      <p> Membro Efetivo da Ordem dos Psicólogos (OPP), Cédula Profissional nº 25648</p>
+      <p> Mestrado Integrado em Psicologia na área de especialização Psicologia Clínica e da Saúde — Psicoterapia Sistémica e Familiar, pela Faculdade de Psicologia e de Ciências da Educação da Universidade de Coimbra — FPCEUC </p>
+      <p> Formação Especializada em Intervenção Sistémica e Familiar pela Sociedade Portuguesa de Terapia Familiar (SPTF) (atualmente em supervisão)</p>
+      <p> Experiência em intervenção familiar no âmbito de processos mandatados de responsabilidades parentais e de promoção e proteção do Tribunal de Família e Menores</p>
+      <p> Desenvolve prática clínica em contexto privado em Psicoterapia Individual (adolescente e adulto), Terapia Familiar e Terapia de Casal (Centro Cirúrgico de Coimbra, Nossa Casa, Workwell)</p>
+      <p> Certificado Internacional de Psicologia Europeu — EuroPsy com o n.º PT-067276-202110 </p>
+      <p> Colaboradora na consulta de Terapia de Casal e Familiar do Centro de Prestação de Serviços à Comunidade da FPCEUC</p>
+      <p> Adjunta de ensino do Mestrado em Psicologia Clínica Sistémica e da Saúde — MPCSS/FPCEUC</p>
+    `,
+    iconLink: "https://www.linkedin.com/in/maria-dias-80a48b162?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" // Unique link for this profile
+  },
+  "ines-sousa": {
+    title: "Inês Sousa",
+    role: "Psicóloga Clínica",
+    description: `
+      <p> Psicóloga — Membro efectivo da Ordem dos Psicólogos Portugueses (OPP), Cédula Profissional n° 24605 </p>
+      <p> Área de especialização — Psicologia da Educação Desenvolvimento e Aconselhamento, Universidade de Coimbra </p>
+      <p> Especialização Avançada — Psicologia Clínica e da Saúde, CRIAP </p>
+      <p> Formação e maior foco nas áreas de gestão de tempo e conflitos, gestão de stress profissional e ansiedade de desempenho </p>
+    `,
+    iconLink: "https://www.linkedin.com/in/in%C3%AAs-sousa-psi?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" // Unique link for this profile
+  },
+  "maria-joao-goncalves": {
+    title: "Maria João Gonçalves",
+    role: "Psicóloga Clínica",
+    description: `
+      <p> Membro efetivo da Ordem dos Psicólogos Portugueses (OPP), Cédula Profissional nº 27982</p>
+      <p> Mestre em Psicologia Clínica e da Saúde — Mestrado em Psicoterapia Sistémica e Familiar — pela Faculdade de Psicologia e Ciências da Educação da Universidade de Coimbra </p>
+      <p> Especialização Avançada em Intervenção na Gravidez e Parentalidade (2024)</p>
+      <p> Experiência profissional em contexto de comunidade terapêutica com intervenção individual e de grupo em adições (químicas e comportamentais) e trauma, ansiedade e depressão</p>
+      <p> Desenvolve prática clínica em contexto privado presencial e online </p>
+    `,
+    iconLink: "https://www.linkedin.com/in/mariajgoncalves?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" // Unique link for this profile
+  },
+  "francisco-marques": {
+    title: "Francisco Marques",
+    role: "Co-fundador e IFE",
+    description: `
+      <p> Mestre em Medicina desde julho 2019 — Mestrado Integrado em Medicina (MIM) na Faculdade de Medicina da Universidade de Coimbra (FMUC)</p>
+      <p> Intercâmbio Clínico da International Federation of Medical Students' Associations (IFMSA), no Serviço de Cirurgia Geral do Hospital Universitário Oswaldo Cruz no Recife, Brasil</p>
+      <p> Obteve 119 valores na Prova Nacional de Admissão ao concurso de acesso ao Internato Médico de Formação Específica de 2019</p>
+      <p> Internato de Formação Geral no Centro Hospitalar de Vila Nova de Gaia/Espinho (CHVNG/E) em 2020</p>
+      <p> Escolheu na 175ª posição a especialidade de Otorrinolaringologia (ORL) no Centro Hospitalar do Tâmega e Sousa (CHTS), onde iniciou funções em janeiro de 2021 e cumpriu o 1º e 2º ano do Internato de Formação Específica</p>
+      <p> Atualmente a exercer funções no serviço de ORL do CHVNG/E (3º-5º ano do Internato de Formação Específica de ORL)</p>
+    `,
+    iconLink: "https://www.linkedin.com/in/francisco-teixeira-marques-096690205/" // Unique link for this profile
+  }
+};
+
+// Function to open the popup
+function openPopup(key) {
+  const teamInfo = teamDetails[key];
+  if (teamInfo) {
+    document.getElementById('popup-title').innerText = teamInfo.title;
+    //document.getElementById('popup-role').innerText = teamInfo.role;
+    document.getElementById('popup-description').innerHTML = teamInfo.description; // Use innerHTML to render HTML content
+
+    // Set the correct link for the icon
+    const iconLinkElement = document.getElementById('popup-icon-link');
+    const iconLink = document.getElementById('popup-icon-link');
+    if (teamInfo.iconLink) {
+      iconLink.href = teamInfo.iconLink; // Set the href to the LinkedIn link
+      iconLink.style.display = 'inline-block'; // Ensure it is visible
+    } else {
+      iconLink.style.display = 'none'; // Hide the link if no LinkedIn link is provided
+    }
+
+
+    document.getElementById('popup').style.display = 'flex';
+  }
+}
+
+// Function to close the popup
+function closePopup() {
+  document.getElementById('popup').style.display = 'none';
+}
+
+
+
+// Add event listeners to team cards
+document.querySelectorAll('.team-cardB').forEach(card => {
+  card.addEventListener('click', () => {
+    const key = card.getAttribute('data-key');
+    openPopup(key);
+  });
+});
+
+/////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////FAQS
+/////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
+
+function toggleFaq(faqElement) {
+  // Check if the element is already expanded
+  const isExpanded = faqElement.classList.contains("expanded");
+
+  // Collapse all other FAQ answers
+  document.querySelectorAll(".cards-faqs").forEach(card => {
+    card.classList.remove("expanded");
+  });
+
+  // Toggle the clicked FAQ
+  if (!isExpanded) {
+    faqElement.classList.add("expanded");
+  }
+}
+
+/////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////Menu Effect
+/////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
+
+// Select all sections
+const sections = document.querySelectorAll('section'); // Ensure your sections have IDs matching the hrefs in the nav
+
+// Select only header navigation links
+const headerNavLinks = document.querySelectorAll('header .nav-li');
+
+// Define a mapping of sections to menu items
+const sectionToNavMap = {
+  services: 'services', // When in #services, highlight the services link
+  rules: 'services',    // When in #rules, also highlight the services link
+  about: 'about',
+  team: 'team',
+  reviews: 'reviews',
+  faqs: 'faqs',
+  contact: 'contact',
+};
+
+// Function to handle the active class
+function activateMenuItem() {
+  let currentSection = '';
+
+  // Check if we're on the blog.html page
+  const isBlogPage = window.location.pathname.includes('blog.html');
+
+  if (isBlogPage) {
+    // Highlight the Blog menu item in the header only
+    headerNavLinks.forEach((link) => {
+      link.classList.remove('active');
+      if (link.getAttribute('href') === 'blog.html') {
+        link.classList.add('active');
+      }
+    });
+    return;
+  }
+
+  // Regular section handling for index.html
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    if (pageYOffset >= sectionTop - sectionHeight / 3) {
+      currentSection = section.getAttribute('id');
+    }
+  });
+
+  // Determine the menu item to highlight based on the section-to-nav mapping
+  const activeMenuItem = sectionToNavMap[currentSection];
+
+  // Update the header navigation links
+  headerNavLinks.forEach((link) => {
+    link.classList.remove('active');
+    if (link.getAttribute('href').includes(`#${activeMenuItem}`)) {
+      link.classList.add('active');
+    }
+  });
+}
+
+// Listen to scroll events
+window.addEventListener('scroll', activateMenuItem);
+
+// Initial call to ensure the correct item is active on page load
+activateMenuItem();
+
+
+/////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////Sidebar Responsive Menu
+/////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
+
+function showSidebar() {
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'flex'
+}
+
+function hideSidebar() {
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'none'
+}
